ff5f7dd2317763a8bdbd6ec61a3fa26f4a929118
CHOIisaac <91372398+CHOIisaac@users.noreply.github.com>
2022-02-15 12:57:40 +0900

    Add files via upload

---
+++ 02.14/Prob05.java
@@ -0,0 +1,27 @@
+package a.base.ex06;
+
+import java.util.Arrays;
+
+//주어진 일차원 배열의 내용을 오름차순으로 정렬하는 프로그램 만들기 - 이중포문 사용하기
+public class Prob05 {
+
+	public static void main(String[] args) {
+		
+		int[] array = { 3, 24, 1, 55, 17, 43, 5 };
+		int tmp = 0;
+		for(int i = 0; i < array.length-1; i++) {
+			
+			for(int j = i+1; j < array.length; j++) {
+				if(array[i]>array[j]) {
+				tmp = array[i];
+				array[i]= array[j];
+				array[j]= tmp; 
+				}
+			}
+		}
+		for(int i = 0; i < array.length; i++) {
+			System.out.print(+array[i]+", ");
+		}
+//	System.out.println(Arrays.toString(array));
+	}//end of main
+}//end of class
---
+++ 02.14/Prob06.java
@@ -0,0 +1,28 @@
+package a.base.ex07;
+
+//주어진 년도의 월에 해당하는 말일을 계산하는 프로그램 만들기. 윤년인 경우 아래
+//1. 4의 배수인 해는 윤년.
+//2. 4의 배수이면서 100의 배수인 해는 윤년인이 아님
+//3. 100의 배수이면서 400의 뱌수인 해는 윤년 
+public class Prob06 {
+
+	public static void main(String[] args) {
+		
+		int year = 400;
+		int month = 2;
+		int maxDay = 0;
+		
+		if(year % 4 == 0 || ( year % 100 ==0 && year % 400 ==0) ){
+			if(month == 2) {
+				maxDay = 29;
+			}else {
+				System.out.println("윤년에 해당하는 달이 아닙니다.");
+				return;
+			}
+		}else {
+			maxDay = 28;
+		}
+			System.out.println(year+"년 "+month+"월의 말일은 "+maxDay+"일 입니다.");
+	}
+
+}
